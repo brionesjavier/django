1242ff09a1e8fb23aa9ef59153ccf2242284d14a
javier briones <javier.brms@gmail.com>
2026-07-28 18:01:14 -0400

    forma antigua concadenar

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("" , views.hello),
-    path("about/" , views.about)
+    path("" , views.index),
+    path("about/" , views.about),
+    path("hello/<str:username>", views.hello)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("Hello world")
+
+def index(request):
+    return HttpResponse("Index page")
+
+def hello(request,username):
+    print(f"saludos {username}")
+    #forma antigua de concadenar
+    return HttpResponse("<h2>Hello %s</h2>" % username)
 
 def about(request):
     return HttpResponse("About")
