93a84a39d2432064055af8ffd78b3f40981b80b3
javier briones <javier.brms@gmail.com>
2026-07-30 10:50:25 -0400

    creando formulario tasks con form

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path("" , views.index, name="home"),
-    path("about/" , views.about,name="about"),
+    path("", views.index, name="home"),
+    path("about/", views.about, name="about"),
     path("hello/<str:username>", views.hello),
-    path("projects/", views.projects,name="projects"),
-    path("tasks/", views.tasks,name="tasks"),
-
+    path("projects/", views.projects, name="projects"),
+    path("tasks/", views.tasks, name="tasks"),
+    path("create_task/", views.create_task, name="create_task"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,27 +1,56 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, JsonResponse
-from myapp.models import Project,Task
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from myapp.models import Project, Task
+
+from .forms import CreateNewTask
 
 # Create your views here.
 
-def index(request):
-    title= "Django Course!!"
-    return render(request, "index.html",{"title":title})
 
-def hello(request,username):
+def index(request):
+    title = "Django Course!!"
+    return render(request, "index.html", {"title": title})
+
+
+def hello(request, username):
     print(f"saludos {username}")
-    #forma antigua de concadenar
+    # forma antigua de concadenar
     return HttpResponse("<h2>Hello %s</h2>" % username)
+
 
 def about(request):
     return render(request, "about.html")
 
+
 def projects(request):
-    #projects = list(Project.objects.values())
+    # projects = list(Project.objects.values())
     projects = Project.objects.all()
-    return render(request, "projects.html",{"projects":projects})
+    return render(request, "projects.html", {"projects": projects})
+
 
 def tasks(request):
     tasks = Task.objects.all()
 
-    return render(request, "tasks.html",{"tasks":tasks})
+    return render(request, "tasks/tasks.html", {"tasks": tasks})
+
+
+def create_task(request):
+
+    if request.method == "GET":
+
+        return render(request,
+                      "tasks/create_task.html",
+                      {"form": CreateNewTask})
+
+    elif request.method == "POST":
+        print(request.POST["title"])
+        print(request.POST["description"])
+
+        Task.objects.create(
+            title=request.POST["title"],
+            description=request.POST["description"],
+            project_id=2,
+        )
+        return redirect("tasks")
+    else:
+        print("metodo no permitidos")
